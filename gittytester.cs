b5e07a73c072e4321d083eadf58a300eea0730b2
testhampster <testhampster@gmail.com>
2017-10-17 12:11:49 +0100

    initial

---
+++ gittytester.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gittytester
+{
+    using System;
+
+    public class Class1
+    {
+        public static string GetCurrency(string cod, SqlConnection DB)
+        {
+            try
+            {
+                String constr = "select distinct currec_symbol from sys_currency_rec where currec_iso_code = '" + cod + "'";
+
+                SqlCommand getsym = new SqlCommand(constr, DB);
+                getsym.CommandTimeout = 120;
+
+                SqlDataReader getrow = getsym.ExecuteReader();
+                string retsym = "c";
+
+                if (getrow.HasRows)
+                {
+
+                    while (getrow.Read())
+                    {
+
+                        logdog("Symbol change to " + (getrow.GetString(0)));
+                        retsym = (getrow.GetString(0));
+
+                        return retsym;
+                    }
+
+                    return null; // massive faff
+                }
+                else
+                {
+                    return null; // massive faff
+                }
+            }
+            catch (SqlException sex)
+            {
+                lib.logdog("Message " + sex.Message + " Number " + sex.Number);
+                lib.logdog("SQL Failure getting Currency " + (sex.ToString()));
+                return null;
+            }
+            catch (Exception ex)
+            {
+                lib.logdog("Failure getting Currency " + DB.ToString() + " " + (ex.ToString()));
+                return null;
+            }
+        }
+    }
+
+}
